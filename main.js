2ec569c40b0fbf146a7463c2af7e045c0e5fc023
Faisal13298 <71083554+Faisal13298@users.noreply.github.com>
2020-12-08 16:29:45 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,17 +2,14 @@ $(document).ready(function(){
 	$("#insertedTask").click(function(){
 		 var text=$(".inputArea").val();
 		 if(text.length > 0){
-		 	$(".pendingTask").append("<li><input type='checkbox' id='checkBox'><span>" + text + "</span><button id='edit'>Edit</button><button id='delete'>Delete</button></li>")
+		 	$(".pendingTask").append("<li><input type='checkbox' id='checkBox'><span>"+text+"</span><button id='edit'>Edit</button><button id='delete'>Delete</button></li>")
 		 	$(".inputArea").val("");
-		 	$(".pendingTask").val('');
 
 		 	$('.pendingTask').on('click','#edit',function(){
-		 		$(this).prev().attr('contenteditable','true');
+		 		$(this).prev().attr('contenteditable','true').focus();
 		 	});
-
 		 	$(".pendingTask").on('click','#delete',function(){
 		 		$(this).parent().remove();
-
 		 	});
 		 	$(".pendingTask").on('click','#checkBox',function(){
 		 		$(this).parent().appendTo(".completedTask");
