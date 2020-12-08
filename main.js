52fa27ba0c6445067cbb3643496277512bcd58a5
Faisal13298 <71083554+Faisal13298@users.noreply.github.com>
2020-12-08 14:02:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+	$("#insertedTask").click(function(){
+		 var text=$(".inputArea").val();
+		 if(text.length > 0){
+		 	$(".pendingTask").append("<li><input type='checkbox' id='checkBox'><span>" + text + "</span><button id='edit'>Edit</button><button id='delete'>Delete</button></li>")
+		 	$(".inputArea").val("");
+		 	$(".pendingTask").val('');
+
+		 	$('.pendingTask').on('click','#edit',function(){
+		 		$(this).prev().attr('contenteditable','true');
+		 	});
+
+		 	$(".pendingTask").on('click','#delete',function(){
+		 		$(this).parent().remove();
+
+		 	});
+		 	$(".pendingTask").on('click','#checkBox',function(){
+		 		$(this).parent().appendTo(".completedTask");
+		 	})
+		 	$(".completedTask").on('click','#delete',function(){
+		 		$(this).parent().remove();
+		 	});
+		 	$(".completedTask").on('click','#checkBox',function(){
+		 		$(this).parent().appendTo(".pendingTask");
+		 	})
+		 }
+	});
+});
